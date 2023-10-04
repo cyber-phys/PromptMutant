7c40164678dadcdeb5e20eca0a2ffd49dd9bab8c
luc <luc@forgotten.name>
2023-10-03 19:37:57 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Promptbreeder
+A basic implementation of Deepmind's Promptbreeder.
+
+[Promptbreeder: Self-Referential Self-Improvement via Prompt Evoultion](https://arxiv.org/pdf/2309.16797.pdf)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='promptbreeder',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/cyber-phys/promptbreeder',
+    license='MIT',
+    author='Luc Chartier',
+    author_email='luc@cyber-phys.com',
+    description='A basic implementation of the Promptbreeder system',
+)
+
